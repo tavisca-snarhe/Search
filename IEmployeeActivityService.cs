2e290db86fe832ea6258d1a0a2a82a2f7a0b33f5
Subhransu Panda <51901786+tavisca-spanda@users.noreply.github.com>
2019-11-28 19:02:44 +0530

    Add files via upload

---
+++ IEmployeeActivityService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Tavisca.Applause
+{
+    public interface IEmployeeActivityService
+    {
+        Task<PagedResult<EmployeeActivity>> GetActivityByEmployeeId(string id, string pageState, int pageSize);
+
+    }
+}
---
+++ IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tavisca.Applause
+{
+    public interface IEmployeeService
+    {
+        Task<PagedResult<Employee>> GetPaginatedEmployees(string pageIdentifier,int pageSize);
+        Task<Employee> GetEmployeeById(string id);
+        Task<PagedResult<Employee>> GetEmployeeByName(string input);
+        Task<List<Employee>> GetTopPerformersByPoint(int limit);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+
+    }
+}
