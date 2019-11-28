20d3e89cc1089ddfe2d8228ab15bd72b3d61417f
Subhransu Panda <51901786+tavisca-spanda@users.noreply.github.com>
2019-11-28 19:00:56 +0530

    Add files via upload

---
+++ GithubActivity.cs
@@ -0,0 +1,8 @@
+﻿namespace Tavisca.Applause
+{
+    public abstract class GitHubActivity
+    {
+        public abstract string ActivityName { get; }
+        public abstract GitHubUser User { get; set; }
+    }
+}
