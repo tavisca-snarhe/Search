fce551849cf003876e32e8f48bd7e0659bd4655c
Subhransu Panda <51901786+tavisca-spanda@users.noreply.github.com>
2019-11-28 18:59:21 +0530

    Add files via upload

---
+++ SessionExpiredException.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Tavisca.Applause
+{
+    public class SessionExpiredException : BaseApplicationException
+    {
+        public SessionExpiredException(string message) : base(message)
+        {
+        }
+
+        public SessionExpiredException(string errorMessage, HttpStatusCode httpStatusCode, List<Info> info = null) : base(errorMessage, httpStatusCode, info)
+        {
+        }
+    }
+}
