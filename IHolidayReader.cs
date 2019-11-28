28936ede152425e2b85ed37970cda08df5ba34a8
Subhransu Panda <51901786+tavisca-spanda@users.noreply.github.com>
2019-11-28 19:04:15 +0530

    Add files via upload

---
+++ IHolidayReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavisca.Applause
+{
+    public interface IHolidayReader
+    {
+        Task<DataTable> Read(Stream stream, string tabName);
+    }
+}
